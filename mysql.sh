2eff5e6db74ae1d042853fc7f1a64821e9d61704
unknown <madhusagardasari@gmail.com>
2024-04-25 01:32:12 +0800

    calling other script

--- mysql.sh
+++ mysql.sh
@@ -20,7 +20,7 @@ systemctl start mysqld &>>$LOGFILE
 VALIDATE $? "start  mysql"
 
 #Below code will be useful for idempotent nature
-mysql -h db.daws78s.online -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
+mysql -h db.vishruth.online -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
 if [ $? -ne 0 ]
 then
     mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOGFILE
